417bedbb10b22e0dc3876ceefb19e7ada70d2f53
Guytho1996 <51941933+Guytho1996@users.noreply.github.com>
2026-06-05 22:49:09 -0500

    feat: agregar PetController

---
+++ src/main/java/com/devops/lab/spring_petclinic_devops/PetController.java
@@ -0,0 +1,13 @@
+package com.devops.lab.spring_petclinic_devops;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PetController {
+
+    @GetMapping("/pets")
+    public String getPets() {
+        return "Hola, esta es la API de mi proyecto DevOps!";
+    }
+}
