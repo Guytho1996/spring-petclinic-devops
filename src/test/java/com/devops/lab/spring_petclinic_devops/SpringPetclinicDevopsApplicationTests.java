7c69778116919d9119f1b2f3d35b8b9e195dab87
Guytho1996 <51941933+Guytho1996@users.noreply.github.com>
2026-06-05 22:28:48 -0500

    First commit: Inicialización de Spring Boot

---
+++ src/test/java/com/devops/lab/spring_petclinic_devops/SpringPetclinicDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.lab.spring_petclinic_devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetclinicDevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
