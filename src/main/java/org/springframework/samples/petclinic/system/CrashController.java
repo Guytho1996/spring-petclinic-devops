ae3c784b5bc59279a693d5fa193fb2c71ac516e0
Antho-321 <allunav@utn.edu.ec>
2026-06-13 18:05:38 +0000

    feat: make CrashController optional and disabled by default via configuration property

--- src/main/java/org/springframework/samples/petclinic/system/CrashController.java
+++ src/main/java/org/springframework/samples/petclinic/system/CrashController.java
@@ -15,6 +15,7 @@
  */
 package org.springframework.samples.petclinic.system;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -26,6 +27,7 @@ import org.springframework.web.bind.annotation.GetMapping;
  * Also see how a view that resolves to "error" has been added ("error.html").
  */
 @Controller
+@ConditionalOnProperty(name = "petclinic.crash-controller.enabled", havingValue = "true")
 class CrashController {
 
 	@GetMapping("/oups")
